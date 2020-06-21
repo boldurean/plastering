deb59610280ad097d2d79c072dda6cbff2570454
admin <v.boldurean@gmail.com>
2020-06-21 17:25:05 +0100

    Added: section price + responsive.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,43 @@ $(document).ready(function(){
         } else {
             headerButton.text("Получить консультацию");
         }
+    });
+
+    //slick
+
+    $('.advantage__row').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('.ul-button').on('click', function () {
+        $(this).hide();
+        $(this).next().addClass('show-list');
     })
+
 
 });
