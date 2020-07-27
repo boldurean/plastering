9c7eaec547ef98ce9b4738c39a2312575050f7e4
admin <v.boldurean@gmail.com>
2020-07-27 05:50:08 +0100

    added footer + responsive and popUP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
 
     const headerButton = $(".header__contacts-button");
+    const modalButton = document.querySelectorAll('[data-toggle="button"]');
+    const modal = document.getElementsByClassName('modal');
+    const forms = document.querySelectorAll('.form');
 
 
     //медиа изменения
@@ -101,5 +104,93 @@ $(document).ready(function () {
         $(this).next().addClass('show-list');
     })
 
+    //Показ модального окна при нажатии на кнопки
 
+    $(modalButton).click(function () {
+        $(modal).toggleClass("active");
+    });
+    $('.modal__close').click(function () {
+        $(modal).removeClass('active');
+    })
+
+    $(document).keydown(function (event) {
+        if (event.keyCode === 27) {
+            $(modal).removeClass('active');
+        }
+    });
+    $(document).click(function (event) {
+        if ($(event.target).is(modal)) {
+            $(modal).removeClass('active');
+        }
+    });
+
+    function initMap() {
+        // The location of Uluru
+        var uluru = {lat: -25.344, lng: 131.036};
+        // The map, centered at
+        var map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 4, center: uluru});
+        // The marker, positioned at Uluru
+        var marker = new google.maps.Marker({position: uluru, map: map});
+    }
+
+    $('[type=tel]').mask('+7(000) 000-00-00');
+
+
+    //validate
+    $("form").each( function() {
+        $(this).validate({
+            errorElement: "div",
+            errorClass: "invalid",
+            rules: {
+                userName: {
+                    required: true,
+                    minlength: 2,
+                    maxlength: 15
+                },
+                userPhone: {
+                    required: true,
+                    minlength: 17
+                },
+                userEmail: {
+                    required: true,
+                    email: true
+                },
+                userMessage: {
+                    required: true,
+                    minlength: 15
+                },
+            },
+            //сообщения ошибок
+            messages: {
+                userName: {
+                    required: "Заполните поле",
+                    minlength: "Имя должно быть не короче 2 букв",
+                    maxlength: "Имя должно быть не длинее 15 букв"
+                },
+                userPhone: {
+                    required: "Заполните поле",
+                    minlength: "Введите полный номер телефона"
+                },
+                userEmail: {
+                    required: "Заполните поле",
+                    email: "Введите корректный Email"
+                },
+                userMessage: {
+                    required: "Задайте Ваш вопрос",
+                    minlength: "сообщение должен быть не короче 15 символов"
+                },
+            },
+            submitHandler: function (form) {
+                $.ajax({
+                    type: 'post',
+                    url: 'send.php',
+                    data: $(form).serialize(),
+                    success: function (response) {
+                        $(form)[0].reset();
+                    }
+                });
+            }
+        })
+    })
 });
---
+++ assets/send.php
@@ -0,0 +1,44 @@
+<?php
+
+$userPhone = $_POST['userPhone'];
+$userName = $_POST['userName'];
+$userEmail = $_POST['userEmail'];
+$userMessage = $_POST['userMessage'];
+
+// Load Composer's autoloader
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth = true;                                   // Enable SMTP authentication
+    $mail->Username = 'GloYnob@gmail.com';                     // SMTP username
+    $mail->Password = 'NH510jGoo4s';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('GloYnob@gmail.com', Василий);
+    $mail->addAddress('yesnookbye@gmail.com');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта';
+    $mail->Body = "Имя пользователя: ${userName}, Его телефон: ${userPhone}, email: -${userEmail}, сообщение: ${userMessage}%";
+
+    if ($mail->send()) {
+        echo "ok";
+    } else {
+        echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+    }
+
+} catch (Exception $e) {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
